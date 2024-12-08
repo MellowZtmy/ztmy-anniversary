b56ac6d05dddf495ef268737ce3abf882dc044e1
MellowZtmy <157828386+MellowZtmy@users.noreply.github.com>
2024-12-09 01:17:48 +0900

    敬称略を記載

--- js/main.js
+++ js/main.js
@@ -372,6 +372,8 @@ function createDisplay(mode, page, sortMode) {
     });
     tag += '         </div>'; //card-list
   }
+  // 敬称略
+  tag += '<div class="right-text">※敬称略</div>';
 
   // ページング作成
   tag += createPagingTag(display);
